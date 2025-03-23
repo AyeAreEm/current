eec8dbf38fb4d7582814fe3b55171aa22356ddcd
AyeAreEm <ag.ayeareem@gmail.com>
2025-03-23 17:22:18 +1000

    update build.bat, hate windows

--- build.bat
+++ build.bat
@@ -1,12 +1,20 @@
 @echo off
-
-set option=%1
-
+shift
+set option=%0
+shift
+set params=%0
+shift
 if "%option%"=="build" (
     odin build src -out:current.exe
 ) else if "%option%"=="run" (
+    :loop
+    if "%0"=="" goto endloop
+    set params=%params% %0
     shift
-    odin run src -out:current.exe -- %*
+    goto loop
+    :endloop
+    echo %params%
+    odin run src -out:current.exe -- %params%
 ) else (
     echo invalid option
 )
